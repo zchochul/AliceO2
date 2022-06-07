607772e8647ccd6ca62e7d841c605b24ba8ce794
zchochul <87480906+zchochul@users.noreply.github.com>
2022-06-07 18:41:07 +0200

    New prod.sh and prod-config.json from Luca

--- prod.sh
+++ prod.sh
@@ -1,1 +1,15 @@
-o2-analysis-cf-femtodream-producer-reduced --configuration json://prod-config.json  --aod-writer-resfile FemtoAO2D  --aod-writer-keep AOD/FEMTODREAMPARTS/0,AOD/FEMTODREAMCOLS/0,AOD/FEMTODEBUGPARTS/0 -b | o2-analysis-timestamp  --configuration json://prod-config.json -b | o2-analysis-multiplicity-table  --configuration json://prod-config.json -b | o2-analysis-event-selection  --configuration json://prod-config.json -b | o2-analysis-trackextension  --configuration json://prod-config.json -b | o2-analysis-trackselection  --configuration json://prod-config.json -b | o2-analysis-pid-tpc  --configuration json://prod-config.json -b |  o2-analysis-pid-tof  --configuration json://prod-config.json -b | o2-analysis-trackextension --configuration json://prod-config.json -b
+#!/bin/bash
+
+config_file=$1
+
+o2-analysis-trackselection --configuration json://$config_file -b | o2-analysis-timestamp  --configuration json://$config_file -b | 
+o2-analysis-multiplicity-table  --configuration json://$config_file -b | 
+o2-analysis-fdd-converter --configuration json://$config_file -b |
+o2-analysis-event-selection  --configuration json://$config_file -b |
+o2-analysis-trackextension  --configuration json://$config_file -b |
+o2-analysis-pid-tpc  --configuration json://$config_file -b |
+o2-analysis-pid-tof-base  --configuration json://$config_file -b |
+o2-analysis-pid-tof  --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
+o2-analysis-lf-lambdakzerobuilder --configuration json://$config_file -b |
+o2-analysis-cf-femtodream-producer --configuration json://$config_file  --aod-writer-resfile FemtoAO2D  --aod-writer-keep AOD/FEMTODREAMPARTS/0,AOD/FEMTODREAMCOLS/0,AOD/FEMTODEBUGPARTS/0 -b
+
