94841296c99c41208c4d89807b9b1e58c5107b70
Zuzanna Chochulska <87480906+zchochul@users.noreply.github.com>
2022-07-27 14:03:13 +0200

    updated scripts and config files

--- prod.sh
+++ prod.sh
@@ -2,9 +2,10 @@
 
 config_file=$1
 
-o2-analysis-trackselection --configuration json://$config_file -b | o2-analysis-timestamp  --configuration json://$config_file -b | 
+o2-analysis-trackselection --configuration json://$config_file -b | 
+o2-analysis-timestamp  --configuration json://$config_file -b | 
 o2-analysis-multiplicity-table  --configuration json://$config_file -b | 
-o2-analysis-fdd-converter --configuration json://$config_file -b |
+#o2-analysis-fdd-converter --configuration json://$config_file -b |
 o2-analysis-event-selection  --configuration json://$config_file -b |
 o2-analysis-trackextension  --configuration json://$config_file -b |
 o2-analysis-pid-tpc  --configuration json://$config_file -b |
@@ -12,4 +13,3 @@ o2-analysis-pid-tof-base  --configuration json://$config_file -b |
 o2-analysis-pid-tof  --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
 o2-analysis-lf-lambdakzerobuilder --configuration json://$config_file -b |
 o2-analysis-cf-femtodream-producer --configuration json://$config_file  --aod-writer-resfile FemtoAO2D  --aod-writer-keep AOD/FEMTODREAMPARTS/0,AOD/FEMTODREAMCOLS/0,AOD/FEMTODEBUGPARTS/0 -b
-
