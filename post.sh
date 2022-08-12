03e139289e8a4fad5b20c4bfd4ab69bbd811925f
Zuzanna Chochulska <87480906+zchochul@users.noreply.github.com>
2022-08-12 13:47:22 +0200

    Add files via upload

--- post.sh
+++ post.sh
@@ -6,7 +6,6 @@ config_file=$1
 #o2-analysis-cf-femtodream-pair-track-track --configuration json://$config_file -b |
 #o2-analysis-cf-femtodream-debug-v0 -b --configuration json://$config_file -b |
 #o2-analysis-cf-femtodream-pair-track-v0 --configuration json://$config_file --aod-memory-rate-limit 600000000 -b
-
-./o2-analysis-cf-femto-world-pair-track-track --configuration json://$config_file --aod-memory-rate-limit 600000000 -b
+o2-analysis-cf-femto-world-pair-track-track --configuration json://$config_file --aod-memory-rate-limit 600000000 -b
 
 
--- prod.sh
+++ prod.sh
@@ -11,5 +11,7 @@ o2-analysis-trackextension  --configuration json://$config_file -b |
 o2-analysis-pid-tpc  --configuration json://$config_file -b |
 o2-analysis-pid-tof-base  --configuration json://$config_file -b |
 o2-analysis-pid-tof  --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
+o2-analysis-pid-tof-beta --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
 o2-analysis-lf-lambdakzerobuilder --configuration json://$config_file -b |
-o2-analysis-cf-femtodream-producer --configuration json://$config_file  --aod-writer-resfile FemtoAO2D  --aod-writer-keep AOD/FEMTODREAMPARTS/0,AOD/FEMTODREAMCOLS/0,AOD/FEMTODEBUGPARTS/0 -b
+o2-analysis-cf-femto-world-producer --configuration json://$config_file  --aod-writer-resfile FemtoAO2D  --aod-writer-keep AOD/FEMTOWORLDPARTS/0,AOD/FEMTOWORLDCOLS/0 -b
+#o2-analysis-cf-femtodream-producer --configuration json://$config_file  --aod-writer-resfile FemtoAO2D  --aod-writer-keep AOD/FEMTODREAMPARTS/0,AOD/FEMTODREAMCOLS/0,AOD/FEMTODEBUGPARTS/0 -b
